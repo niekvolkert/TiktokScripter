e6cdf8993502e609543e1edb6f5d6bcc21d53126
niekvolkert <100355204+niekvolkert@users.noreply.github.com>
2024-11-04 23:33:33 +0000

    Error changes

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,5 +1,5 @@
-import { Header } from '@/components/layout/header.tsx'
-import { Footer } from '@/components/layout/footer.tsx'
+import { Header } from '@/components/layout/header'
+import { Footer } from '@/components/layout/footer'
 
 export default function MarketingLayout({
   children,
